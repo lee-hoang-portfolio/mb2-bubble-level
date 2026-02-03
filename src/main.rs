3b0c9153d7e82c2dd691d9612ac7b41750dc0e39
Lee Hoang <leehoang97@gmail.com>
2026-02-03 00:12:25 -0800

    set up use statements

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
-use microbit::board::Board; // https://docs.rs/microbit-v2/0.16.0/microbit/
+use microbit::{board::Board, display::blocking::Display}; // https://docs.rs/microbit-v2/0.16.0/microbit/
 
 // Docs: https://docs.rs/lsm303agr/1.1.0/lsm303agr/
+use lsm303agr::Acceleration;
 
 // =================================================
 
