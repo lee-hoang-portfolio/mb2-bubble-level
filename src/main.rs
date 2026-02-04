eb1c54a51e0d44f552510e49cec74a5ac9ae45c2
Lee Hoang <leehoang97@gmail.com>
2026-02-03 23:22:03 -0800

    replace placeholder code

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
-use microbit::{board::Board, display::blocking::Display}; // https://docs.rs/microbit-v2/0.16.0/microbit/
+use microbit::{board::Board, display::blocking::Display, hal::timer::Timer}; // https://docs.rs/microbit-v2/0.16.0/microbit/
 
 // Docs: https://docs.rs/lsm303agr/1.1.0/lsm303agr/
 use lsm303agr::Acceleration;
@@ -19,9 +19,19 @@ use lsm303agr::Acceleration;
 fn main() -> ! {
     rtt_init_print!();
     let _board = Board::take().unwrap();
-    let mut counter = 0u64;
+    let mut display = Display::new(_board.display_pins);
+    let mut timer = Timer::new(_board.TIMER0);
+
+    let level = [
+        [0u8, 0u8, 0u8, 0u8, 0u8],
+        [0u8, 0u8, 0u8, 0u8, 0u8],
+        [0u8, 0u8, 1u8, 0u8, 0u8],
+        [0u8, 0u8, 0u8, 0u8, 0u8],
+        [0u8, 0u8, 0u8, 0u8, 0u8],
+    ];
+
     loop {
-        rprintln!("{}", counter);
-        counter += 1;
+        display.show(&mut timer, level, 1000);
+        rprintln!("Level");
     }
 }
