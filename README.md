69c90e7034180d75e8b46006c9c025700d2fe1ff
Lee Hoang <leehoang97@gmail.com>
2026-02-04 22:59:10 -0800

    set up i2c part 1

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 # What I did
 
-TBD
+I created a Microbit V2 program to simulate a [bubble level](https://en.wikipedia.org/wiki/Spirit_level).
 
 # How it went
 
@@ -15,4 +15,6 @@ TBD
 
 # Acknowledgements
 
-- TBD
+- Documentation for the `microbit-v2`, `lsm303agr`, and other crates. Links can be found in the source code.
+- [MB2 Discovery Book](https://docs.rust-embedded.org/discovery-mb2/index.html) - Especially Chapter 11. Provided starting points for code.
+- [pdx-cs-rust-embedded](https://github.com/pdx-cs-rust-embedded) - Provided starting points for setting up the project.
--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,54 @@ use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
-use microbit::{board::Board, display::blocking::Display, hal::timer::Timer}; // https://docs.rs/microbit-v2/0.16.0/microbit/
+
+// Microbit functions
+// https://docs.rs/microbit-v2/0.16.0/microbit/
+use microbit::{
+    board::Board, 
+    display::blocking::Display, 
+    hal::{
+        pac::twim0::frequency::FREQUENCY_A, 
+        timer::Timer, 
+        twim::Twim
+    }
+}; 
 
 // Docs: https://docs.rs/lsm303agr/1.1.0/lsm303agr/
-use lsm303agr::Acceleration;
+use lsm303agr::{
+    Acceleration, 
+    Lsm303agr
+};
 
 // =================================================
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let _board = Board::take().unwrap();
-    let mut display = Display::new(_board.display_pins);
-    let mut timer = Timer::new(_board.TIMER0);
 
+    // set up the board, timer, and display
+    let _board = Board::take().unwrap();
+    let mut timer = Timer::new(_board.TIMER0);
+    let mut display = Display::new(_board.display_pins);
+
+    // set up the i2c - it contains a TWIM object.
+    // Based on https://docs.rust-embedded.org/discovery-mb2/12-i2c/using-a-driver.html
+    let i2c = {
+        Twim::new(
+            _board.TWIM0, // board twim
+            _board.i2c_internal.into(), // board's internal i2c pins
+            FREQUENCY_A::K100 // frequency is 100 kbps
+        )
+
+    };
+
+    // set up the sensor using the i2c
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+
+    // TBD
+
+    // default display - shows a dot in the middle
     let level = [
         [0u8, 0u8, 0u8, 0u8, 0u8],
         [0u8, 0u8, 0u8, 0u8, 0u8],
@@ -30,8 +64,12 @@ fn main() -> ! {
         [0u8, 0u8, 0u8, 0u8, 0u8],
     ];
 
+    // loop
     loop {
+        // placeholder: show the item
         display.show(&mut timer, level, 1000);
         rprintln!("Level");
+    
+        // TBD
     }
 }
